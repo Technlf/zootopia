7b5fa38e43c15104a9e408fbe88c69e13f052d26
Jens M. <jensmayer89@gmail.com>
2025-06-12 13:10:15 +0200

    Add the print animals function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,6 +6,19 @@ def load_animals_data(file_path):
     with open(file_path, "r") as fileobject:
         return js.load(fileobject)
 
-animals_data = load_animals_data('animals_data.json')
 
+def print_animals_data(animals_data):
+    """This function prints te animals data."""
+    animals_data = load_animals_data("animals_data.json")
+    for animal in animals_data:
+        animal_name = animal['name']
+        animal_diet = animal['characteristics']['diet']
+        animal_location = animal['locations'][0]
+        animal_type = animal['characteristics']['type']
+        print(f"\nName: {animal_name}"
+              f"\n Diet: {animal_diet}"
+              f"\n Location: {animal_location}"
+              f"\n Type: {animal_type}")
+
+print_animals_data(load_animals_data("animals_data.json"))
 
